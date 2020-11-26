fe1f7e0f0f0bc865e4f2a759a444723bece4680c
Perfspec <lewisrwebb@gmail.com>
2020-11-26 01:01:52 +0000

    add: PassengerWeights struct and learning_rate

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,11 +3,13 @@ use kaggle_c_titanic::Config;
 
 fn main() {
 	// Argument 1: Any String
-	// Argument 2: Path to the Training Data
+	// Argument 2: Learning Rate of Gradient Descent
 	// Argument 3: Path to the Training Data
-	// Argument 4: Path where the Output Data will be created
+	// Argument 4: Path to the Test Data
+	// Argument 5: Path where the Output Data will be created
     let args: Vec<String> = vec![
 		"This vector can be loaded using std::env::args().collect() too.".to_string(),
+		"0.1".to_string(),
 		"data/train.csv".to_string(),
 		"data/test.csv".to_string(),
 		"output.csv".to_string()];
--- src/lib.rs
+++ src/lib.rs
@@ -4,269 +4,254 @@ use serde::Deserialize;
 use std::collections::HashMap;
 
 pub struct Config {
+    pub learning_rate: f64,
     pub training_data_filename: String,
-	pub test_data_filename: String,
-	pub output_filename: String,
+    pub test_data_filename: String,
+    pub output_filename: String,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 4 {
+        if args.len() < 5 {
             return Err("not enough arguments");
         }
-        let training_data_filename = args[1].clone();
-		let test_data_filename = args[2].clone();
-		let output_filename = args[3].clone();
-		
-		Ok(Config {training_data_filename, test_data_filename, output_filename})
+        let learning_rate_string = args[1].clone();
+        let training_data_filename = args[2].clone();
+        let test_data_filename = args[3].clone();
+        let output_filename = args[4].clone();
+        
+        match learning_rate_string.parse::<f64>().unwrap() {
+            Ok(learning_rate) => Ok(Config {learning_rate, training_data_filename, test_data_filename, output_filename}),
+            Err(_) => Err("unable to parse learning rate"),
+        }
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    //Read training_data into vector of training_passengers, which will be reused many times.
     let mut training_data = Reader::from_path(config.training_data_filename)?;
-	
-	// Initialize pmfs
-	let mut passenger_class_pmf = HashMap::new();
-	let mut sex_pmf = HashMap::new();
-	let mut age_pmf = HashMap::new();
-	let mut siblings_spouses_pmf = HashMap::new();
-	
-	for result in training_data.deserialize() {
-        let record: TrainingRecord = result?;
-        record.update_passenger_class_pmf(&mut passenger_class_pmf);
-		record.update_sex_pmf(&mut sex_pmf);
-		record.update_age_pmf(&mut age_pmf);
-		record.update_siblings_spouses_pmf(&mut siblings_spouses_pmf);
+    let mut training_passengers = Vec::new();
+    
+    for result in training_data.deserialize() {
+        let mut training_passenger: TrainingPassenger = result?;
+        training_passengers.push(training_passenger);
     }
-	
-	println!("passenger_class_pmf: {:?}", passenger_class_pmf);
-	println!("sex_pmf: {:?}", sex_pmf);
-	println!("age_pmf: {:?}", age_pmf);
-	println!("siblings_spouses_pmf: {:?}", siblings_spouses_pmf);
-	
+    
+    // Initialize weights
+    let mut passenger_weights = PassengerWeights::new();
+    
+    
     Ok(())
 }
 
 #[derive(Debug, Deserialize)]
 enum Survived {
-	#[serde(rename = "0")]
-	Yes,
-	
-	#[serde(rename = "1")]
-	No
+    #[serde(rename = "0")]
+    Yes,
+    
+    #[serde(rename = "1")]
+    No
 }
 
 #[derive(Debug, Deserialize)]
 enum PassengerClass {
-	#[serde(rename = "1")]
-	First,
-	
-	#[serde(rename = "2")]
-	Second,
-	
-	#[serde(rename = "3")]
-	Third,
+    #[serde(rename = "1")]
+    First,
+    
+    #[serde(rename = "2")]
+    Second,
+    
+    #[serde(rename = "3")]
+    Third,
 }
 
 #[derive(Debug, Deserialize)]
 enum Sex {
-	#[serde(rename = "male")]
-	Male,
-	
-	#[serde(rename = "female")]
-	Female
+    #[serde(rename = "male")]
+    Male,
+    
+    #[serde(rename = "female")]
+    Female
 }
 
 #[derive(Debug, Deserialize)]
 enum PortOfEmbarkation {
-	#[serde(rename = "C")]
-	Cherbourg,
-	
-	#[serde(rename = "S")]
-	Southampton,
-	
-	#[serde(rename = "Q")]
-	Queenstown,
+    #[serde(rename = "C")]
+    Cherbourg,
+    
+    #[serde(rename = "S")]
+    Southampton,
+    
+    #[serde(rename = "Q")]
+    Queenstown,
 }
 
 #[derive(Debug, Deserialize)]
-struct TrainingRecord {
-	#[serde(rename = "PassengerId")]
+struct TrainingPassenger {
+    #[serde(rename = "PassengerId")]
     passenger_id: u64,
-	
-	#[serde(rename = "Survived")]
+    
+    #[serde(rename = "Survived")]
     survived: Survived,
-	
-	#[serde(rename = "Pclass")]
+    
+    #[serde(rename = "Pclass")]
     passenger_class: Option<PassengerClass>,
-	
-	#[serde(rename = "Name")]
+    
+    #[serde(rename = "Name")]
     name: Option<String>,
-	
-	#[serde(rename = "Sex")]
+    
+    #[serde(rename = "Sex")]
     sex: Option<Sex>,
-	
-	#[serde(rename = "Age")]
+    
+    #[serde(rename = "Age")]
     age: Option<f64>,
-	
-	#[serde(rename = "SibSp")]
+    
+    #[serde(rename = "SibSp")]
     siblings_spouses: Option<u8>,
-	
-	#[serde(rename = "Parch")]
+    
+    #[serde(rename = "Parch")]
     parents_children: Option<u8>,
-	
-	#[serde(rename = "Ticket")]
+    
+    #[serde(rename = "Ticket")]
     ticket_id: Option<String>,
-	
-	#[serde(rename = "Fare")]
+    
+    #[serde(rename = "Fare")]
     fare: Option<f64>,
-	
-	#[serde(rename = "Cabin")]
+    
+    #[serde(rename = "Cabin")]
     cabin_id: Option<String>,
-	
-	#[serde(rename = "Embarked")]
+    
+    #[serde(rename = "Embarked")]
     port_of_embarkation: Option<PortOfEmbarkation>,
-}
-
-impl TrainingRecord {
-	fn update_passenger_class_pmf(&self, pmf: &mut HashMap<String, u64>) {
-		match self.passenger_class {
-			None => (),
-			Some(PassengerClass::First) => {
-				update_string_pmf(&"first".to_string(), pmf);
-			},
-			Some(PassengerClass::Second) => {
-				update_string_pmf(&"second".to_string(), pmf);
-			},
-			Some(PassengerClass::Third) => {
-				update_string_pmf(&"third".to_string(), pmf);
-			},
-		}
-	}
-	
-	fn update_sex_pmf(&self, pmf: &mut HashMap<String, u64>) {
-		match self.sex {
-			None => (),
-			Some(Sex::Male) => {
-				update_string_pmf(&"male".to_string(), pmf);
-			},
-			Some(Sex::Female) => {
-				update_string_pmf(&"female".to_string(), pmf);
-			},
-		}
-	}
-	
-	fn update_age_pmf(&self, pmf: &mut HashMap<u64, u64>) {
-		match self.age {
-			None => (),
-			Some(age) => {
-				let rounded_age = age.round() as u64;
-				update_u64_pmf(&rounded_age, pmf);
-			}
-		}
-	}
-	
-	fn update_siblings_spouses_pmf(&self, pmf: &mut HashMap<u8, u64>) {
-		match self.siblings_spouses {
-			None => (),
-			Some(siblings_spouses) => {
-				update_u8_pmf(&siblings_spouses, pmf);
-			}
-		}
-	}
-	
-	fn update_parents_children_pmf(&self, pmf: &mut HashMap<u8, u64>) {
-		match self.parents_children {
-			None => (),
-			Some(parents_children) => {
-				update_u8_pmf(&parents_children, pmf);
-			}
-		}
-	}
-	
-	fn update_fare_pmf(&self, pmf: &mut HashMap<u64, u64>) {
-		match self.fare {
-			None => (),
-			Some(fare) => {
-				let rounded_fare = fare.round() as u64;
-				update_u64_pmf(&rounded_fare, pmf);
-			}
-		}
-	}
-	
-	
-}
-
-fn update_string_pmf(key: &String, pmf: &mut HashMap<String, u64>) {
-	let count = pmf.entry(key.clone()).or_insert(0);
-	*count += 1;
-}
-
-fn update_u64_pmf(key: &u64, pmf: &mut HashMap<u64, u64>) {
-	let count = pmf.entry(*key).or_insert(0);
-	*count += 1;
-}
-
-fn update_u8_pmf(key: &u8, pmf: &mut HashMap<u8, u64>) {
-	let count = pmf.entry(*key).or_insert(0);
-	*count += 1;
 }
 
 #[derive(Debug, Deserialize)]
-struct Record {
-	#[serde(rename = "PassengerId")]
+struct Passenger {
+    #[serde(rename = "PassengerId")]
     passenger_id: u64,
-	
-	#[serde(rename = "Pclass")]
+    
+    #[serde(rename = "Pclass")]
     passenger_class: Option<PassengerClass>,
-	
-	#[serde(rename = "Name")]
+    
+    #[serde(rename = "Name")]
     name: Option<String>,
-	
-	#[serde(rename = "Sex")]
+    
+    #[serde(rename = "Sex")]
     sex: Option<Sex>,
-	
-	#[serde(rename = "Age")]
+    
+    #[serde(rename = "Age")]
     age: Option<f64>,
-	
-	#[serde(rename = "SibSp")]
+    
+    #[serde(rename = "SibSp")]
     siblings_spouses: Option<u8>,
-	
-	#[serde(rename = "Parch")]
+    
+    #[serde(rename = "Parch")]
     parents_children: Option<u8>,
-	
-	#[serde(rename = "Ticket")]
+    
+    #[serde(rename = "Ticket")]
     ticket_id: Option<String>,
-	
-	#[serde(rename = "Fare")]
+    
+    #[serde(rename = "Fare")]
     fare: Option<f64>,
-	
-	#[serde(rename = "Cabin")]
+    
+    #[serde(rename = "Cabin")]
     cabin_id: Option<String>,
-	
-	#[serde(rename = "Embarked")]
+    
+    #[serde(rename = "Embarked")]
     port_of_embarkation: Option<PortOfEmbarkation>,
+}
+
+#[derive(Debug)]
+struct PassengerWeights {
+    bias: f64,
+    passenger_class: Vec<f64>,
+    name: Vec<f64>,
+    sex: Vec<f64>,
+    age: Vec<f64>,
+    siblings_spouses: Vec<f64>,
+    parents_children: Vec<f64>,
+    ticket_id: Vec<f64>,
+    fare: Vec<f64>,
+    cabin_id: Vec<f64>,
+    port_of_embarkation: Vec<f64>,
+}
+
+impl PassengerWeights {
+    fn new() -> PassengerWeights {
+        let mut bias = 1_f64;
+        
+        //Optional floats, integers and strings only have one param instantiated for when Optional matches None.
+        //When Optional matches Some(value), then extra capacity will be reserved at runtime.
+        let mut name = Vec::new();
+        name.push(1_f64);
+        
+        let mut age = Vec::new();
+        age.push(1_f64);
+        
+        let mut siblings_spouses = Vec::new();
+        siblings_spouses.push(1_f64);
+        
+        let mut parents_children = Vec::new();
+        parents_children.push(1_f64);
+        
+        let mut ticket_id = Vec::new();
+        ticket_id.push(1_f64);
+        
+        let mut cabin_id = Vec::new();
+        cabin_id.push(1_f64);
+        
+        //Optional enums can be instantiated now, because they have a maximum number of weights.
+        //One for each value in the enum when Optional matches Some and one for when Optional matches None.
+        let mut passenger_class = Vec::with_capacity(4);
+        for i in passenger_class {
+            i=1_f64;
+        }
+        
+        let mut sex = Vec::with_capacity(3);
+        for i in sex {
+            i=1_f64;
+        }
+                
+        let mut port_of_embarkation = Vec::with_capacity(4);
+        for i in port_of_embarkation {
+            i=1_f64;
+        }
+                
+        PassengerWeights {
+            bias,
+            passenger_class,
+            name,
+            sex,
+            age,
+            siblings_spouses,
+            parents_children,
+            ticket_id,
+            fare,
+            cabin_id,
+            port_of_embarkation,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
+    use super::*;
+    
     #[test]
-	#[should_panic(expected = "not enough arguments")]
+    #[should_panic(expected = "not enough arguments")]
     fn when_less_than_four_arguments_then_return_error() {
-		let args = vec!["first".to_string(), "second".to_string()];
+        let args = vec!["first".to_string(), "second".to_string()];
         Config::new(&args).unwrap();
     }
-	
-	#[test]
-	fn when_at_least_three_arguments_then_create_config() {
-		let args = vec!["first".to_string(), "second".to_string(), "third".to_string(), "fourth".to_string()];
+    
+    #[test]
+    fn when_at_least_three_arguments_then_create_config() {
+        let args = vec!["first".to_string(), "second".to_string(), "third".to_string(), "fourth".to_string()];
         let conf = Config::new(&args).unwrap();
-		let mut sum = String::new();
-		sum.push_str(&conf.training_data_filename);
-		sum.push_str(&conf.test_data_filename);
-		sum.push_str(&conf.output_filename);
-		assert_eq!(&sum, "secondthirdfourth");
+        let mut sum = String::new();
+        sum.push_str(&conf.training_data_filename);
+        sum.push_str(&conf.test_data_filename);
+        sum.push_str(&conf.output_filename);
+        assert_eq!(&sum, "secondthirdfourth");
     }
 }
